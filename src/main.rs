72bda28dcf956117b862baca0951e08c5e9f9c83
Niklas Halonen <kahvikannu@gmail.com>
2019-01-08 12:39:07 +0200

    Add uniform windowsize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#[macro_use]
 extern crate glium;
 
 fn main() {
+    use glium::glutin::WindowEvent::*;
     use glium::{glutin, Surface};
 
     let mut events_loop = glium::glutin::EventsLoop::new();
@@ -8,18 +10,78 @@ fn main() {
     let context = glium::glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices =
+        glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+    let fragment_shader_src = r#"
+        #version 140
+
+        uniform vec2 windowSize;
+        out vec4 color;
+
+        void main() {
+            color = vec4(gl_FragCoord.x/windowSize.x, gl_FragCoord.y/windowSize.y, gl_FragCoord.z, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(
+        &display,
+        vertex_shader_src,
+        fragment_shader_src,
+        None,
+    )
+    .unwrap();
+
     let mut closed = false;
     while !closed {
         let mut target = display.draw();
+        let (width, height) = target.get_dimensions();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                // &glium::uniforms::EmptyUniforms,
+                &uniform! {windowSize: [width as f32, height as f32]},
+                &Default::default(),
+            )
+            .unwrap();
         target.finish().unwrap();
 
         events_loop.poll_events(|ev| match ev {
             glutin::Event::WindowEvent { event, .. } => match event {
-                glutin::WindowEvent::CloseRequested => closed = true,
+                CloseRequested => closed = true,
+                CursorMoved { position, .. } => println!("{:?}", position),
                 _ => (),
             },
-            x => println!("{:?}", x),
+            _ => (),
         });
     }
 }
