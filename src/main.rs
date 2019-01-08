4f24d6b217fbc25d0aa0043dd63fed8db67a1b27
Niklas Halonen <kahvikannu@gmail.com>
2019-01-08 09:30:27 +0200

    Add glium

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::{glutin, Surface};
+
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new();
+    let context = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let mut closed = false;
+    while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        events_loop.poll_events(|ev| match ev {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::CloseRequested => closed = true,
+                _ => (),
+            },
+            x => println!("{:?}", x),
+        });
+    }
 }
